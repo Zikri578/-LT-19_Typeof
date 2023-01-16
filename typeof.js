cfd8875c8d4d22e72ffe8acac7094b88328a2575
MuhammadZikri <zikrim577@gmail.com>
2023-01-16 13:28:46 +0700

    Javascript - TypeOf

---
+++ typeof.js
@@ -0,0 +1,33 @@
+// tipe data string
+let data = "hallo";
+
+// tipe data number 
+let angka = 23;
+// tipe number
+let kata = NaN;
+
+// tipe data boolean
+let logika = true;
+
+// tipe data object 
+let a = {};
+
+// tipe data objek
+let b = [];
+
+// tipe data undefined
+let y;
+
+// tipe data bigInt
+const hexaDecimal = 9007199254740991n
+
+// tipe data function
+let c = function () { };
+
+// tipe data objek
+let d = null;
+if (typeof d == "angka") {
+    document.writeln("number");
+} else {
+    document.writeln("objek");
+}
